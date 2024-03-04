80e4264070cdcb39b5e364e32d3e3a939cd20b7b
iitzdante <desouza16julio@gmail.com>
2024-03-05 00:56:27 +0100

    Updated the entire code. Changed the API provider for more infos

--- iplocator.py
+++ iplocator.py
@@ -1,6 +1,6 @@
 import json
 import sys
-
+from datetime import datetime
 import requests
 
 # Check if the IP address is provided as an argument
@@ -11,16 +11,46 @@ if len(sys.argv) < 2:
 # Get the IP address from the command-line argument
 IP = sys.argv[1]
 
-response = requests.get(f'http://api.db-ip.com/v2/free/{IP}')
+response = requests.get(f'https://ipapi.co/{IP}/json/')
 data = response.json()
+
+latitude = data.get("latitude")
+longitude = data.get("longitude")
 
 with open("location.json", "a") as write_file:
     json.dump(data, write_file, indent=4)
 
-print("*" * 50)
-print("Continent:      ", data.get("continentName"))
-print("Country code:   ", data.get("countryCode"))
-print("Country:        ", data.get("countryName"))
-print("State/Province: ", data.get("stateProv"))
-print("City:           ", data.get("city"))
-print("*" * 50)
+dt = datetime.now()
+print("*" * 40)
+print("Date and time is: ",dt.strftime("%Y-%m-%d %H:%M:%S"))
+print("*" * 40)
+
+print("IP              *", data.get("ip"))
+print("Network:        *", data.get("network"))
+print("Network version *", data.get("version"))
+print("Country code:   *", data.get("country_code"))
+print("Country:        *", data.get("country_name"))
+print("Region:         *", data.get("region"))
+print("City:           *", data.get("city"))
+print("Latitude:       *", data.get("latitude"))
+print("Longitude:      *", data.get("longitude"))
+print("Phone code:     *", data.get("country_calling_code"))
+print("Organisation:   *", data.get("org"))
+print("Currency:       *", data.get("currency"))
+print("Currency name:  *", data.get("currency_name"))
+print("Continent Code  *", data.get("continent_code"))
+
+print("*" * 40)
+print()
+
+def google_map(latitude, longitude):
+    if latitude or longitude is None:
+        print("No google map link available.")
+        sys.exit(1)
+    else:
+        map_url = f"https://maps.google.com/?q={latitude},{longitude}"
+        print("Google map link:", map_url)
+        print()
+
+
+google_map(latitude,longitude)
